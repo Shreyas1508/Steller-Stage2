d48b33667939ca58a4d96f0f1ea77b1bd9959826
Shreyas1508 <84567609+Shreyas1508@users.noreply.github.com>
2021-06-28 21:46:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator}from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-ions/Ionicons';
+
+import Feed from "./screens/feed";
+import CreatePost from "./screens/createPost";
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Feed') {
+              iconName = focused
+                ? 'book'
+                : 'book-outline';
+            } else if (route.name === 'CreateStory') {
+              iconName = focused ? 'create' : 'create-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Feed" component={Feed} />
+        <Tab.Screen name="CreatePost" component={CreatePost} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
